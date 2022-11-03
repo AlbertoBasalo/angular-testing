9c4820a1e0158e280e3c2d975235bc1eddf3053b
Alberto Basalo <albertobasalo@hotmail.com>
2022-11-03 10:39:15 +0100

    feat: pico css

---
+++ docs/CLI.md
@@ -0,0 +1,15 @@
+# Angular Laboratory
+
+```bash
+# generate a new project
+npx @angular/cli new angulab  --routing=true --style=css
+# add eslint with prettier
+npm i -D eslint
+# plugin and parser for typescript
+npm i -D @typescript-eslint/eslint-plugin @typescript-eslint/parser
+# prettier interaction
+npm i -D eslint-config-prettier eslint-plugin-prettier
+# Add a bit of style
+npm install @picocss/pico
+# angular.json : "./node_modules/@picocss/pico/css/pico.fluid.classless.min.css",
+```
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <header>
+      <h1>{{ title }}</h1>
+    </header>
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+    <footer>
+      <h6>{{ title }}</h6>
+      <p>Welcome on board</p>
+      <a href="https://twitter.com/albertobasalo">Alberto Basalo</a>
+    </footer>
+  `,
+  styles: [],
 })
 export class AppComponent {
   title = 'angulab';
