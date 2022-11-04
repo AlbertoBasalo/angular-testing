04a94179e91a5103fc592bf8fdf6713460a23ed2
Alberto Basalo <albertobasalo@hotmail.com>
2022-11-04 17:14:10 +0100

    feat: booking interface

--- src/app/models/booking.interface.ts
+++ src/app/models/booking.interface.ts
@@ -1,2 +1,15 @@
 export interface Booking {
+  id: string;
+  tripId: string;
+  customer: {
+    name: string;
+    email: string;
+    phone: string;
+    gender: string;
+  };
+  seats: number;
+  premiumFood: boolean;
+  paymentMethod: string;
+  date: string;
+  status: string;
 }
--- src/app/routes/book/book.module.ts
+++ src/app/routes/book/book.module.ts
@@ -1,19 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { BookRoutingModule } from './book-routing.module';
 import { BookComponent } from './book.component';
 import { BookForm } from './book.form';
 
-
 @NgModule({
-  declarations: [
-    BookComponent,
-    BookForm
-  ],
-  imports: [
-    CommonModule,
-    BookRoutingModule
-  ]
+  declarations: [BookComponent, BookForm],
+  imports: [CommonModule, BookRoutingModule, ReactiveFormsModule],
 })
-export class BookModule { }
+export class BookModule {}
