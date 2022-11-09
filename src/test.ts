53192f17a4e1b037dd891bd1687b6d8f577f6772
Alberto Basalo <albertobasalo@hotmail.com>
2022-11-09 13:43:19 +0100

    fix: test zone import

--- src/test.ts
+++ src/test.ts
@@ -2,13 +2,13 @@
 
 // ! added to use fakeAsync
 import 'zone.js';
+import 'zone.js/testing';
 
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
-import 'zone.js/testing';
 
 declare const require: {
   context(
