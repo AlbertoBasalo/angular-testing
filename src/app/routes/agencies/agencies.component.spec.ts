12e61d07ab2b586ad153e49d2d79b2e4b3b682be
Alberto Basalo <albertobasalo@hotmail.com>
2022-11-16 13:40:22 +0100

    chore: pre agencies component testing

--- src/app/routes/agencies/agencies.component.spec.ts
+++ src/app/routes/agencies/agencies.component.spec.ts
@@ -1,14 +1,13 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { Agency } from '@models/agency.interface';
 import { ApiService } from '@services/api.service';
 import { of } from 'rxjs';
 
 import { AgenciesComponent } from './agencies.component';
 
-// ToDo: use IoC to inject an ApiService double
-
-fdescribe('The Agencies Component _semi-integrated_', () => {
+describe('The Agencies Component _semi-integrated_', () => {
   let component: AgenciesComponent;
   let fixture: ComponentFixture<AgenciesComponent>;
   let apiService: ApiService; // ! the real ApiService
@@ -49,3 +48,56 @@ fdescribe('The Agencies Component _semi-integrated_', () => {
     expect(apiService.getAgencies$).toHaveBeenCalled();
   });
 });
+
+fdescribe('The Agencies Component _presentation_', () => {
+  let component: AgenciesComponent;
+  let fixture: ComponentFixture<AgenciesComponent>;
+  let apiServiceStub: any;
+  const inputAgencies: Agency[] = [
+    {
+      id: 'space-x',
+      name: 'SpaceX',
+      range: 'Interplanetary',
+      status: 'Active',
+    },
+    {
+      id: 'blue-origin',
+      name: 'Blue Origin',
+      range: 'Orbital',
+      status: 'Active',
+    },
+  ];
+  beforeEach(async () => {
+    apiServiceStub = {
+      getAgencies$: () => of(inputAgencies),
+      getOptions$: (r: string) => of([]),
+      postAgency$: (a: Agency) => of(a),
+      deleteAgency$: () => of({}),
+    };
+    await TestBed.configureTestingModule({
+      declarations: [AgenciesComponent],
+      imports: [FormsModule, HttpClientTestingModule],
+      providers: [{ provide: ApiService, useValue: apiServiceStub }],
+    }).compileComponents();
+    fixture = TestBed.createComponent(AgenciesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  it('should present a table with agencies', () => {
+    // Arrange
+    // Act
+    const actualTable = fixture.nativeElement.querySelector('table');
+    // Assert
+    expect(actualTable).toBeTruthy();
+    let actualBodyRows = fixture.nativeElement.querySelectorAll('tbody>tr');
+    const actualBodyRowsLength = actualBodyRows.length;
+    const expectedBodyRowsLength = inputAgencies.length;
+    expect(actualBodyRowsLength).toBe(expectedBodyRowsLength);
+  });
+});
+
+// it should call onDeleteClick on delete button click
+
+// it should allow to fill the form
+
+// it should call onSaveClick on save button click
